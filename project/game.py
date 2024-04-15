19db96ff1206d0d5574826e06084d716508f846d
Thomas Luijkman <thomasluijkman@gmail.com>
2024-04-15 15:05:24 +0200

    very basic implementation of interactive blackjack game

--- project/game.py
+++ project/game.py
@@ -14,8 +14,6 @@ class Card:
     Rank -- 1 = ace, 11 = jack, 12 = queen, 13 = king (all other numbers are obvious)
     """
     def __init__(self, suit, rank):
-        if rank > 13 or rank < 1:
-            raise ValueError(f"Rank value of {rank} is invalid.")
         self.suit = suit
         self.rank = rank
     
@@ -48,13 +46,15 @@ class Card:
         return suit+rank
     
 class Hand:
-    def __init__(self):
-        self.hand = []
+    def __init__(self, hand=[]):
+        self.hand = hand
     
     def addCard(self, card):
+        """Adds a card to the hand"""
         self.hand.append(card)
     
     def aces(self):
+        """Counts the number of aces in a hand"""
         no_aces = 0
         for card in self.hand:
             if card.rank == ACE:
@@ -62,43 +62,201 @@ class Hand:
         return no_aces
     
     def scoreHand(self):
+        """Returns the highest score of the current hand as long as it is under 21 (if possible)"""
         score = 0
         for card in self.hand:
             if card.rank == ACE:
                 score += 11
             else:
                 score += min(card.rank, 10)
-        return score
-    
-    def isBlackjack(self):
-        return self.scoreHand == 21 and len(self.hand) == 2
-    
-    def isBust(self):
-        score = self.scoreHand()
         for _ in range(self.aces(),0,-1):
             if score > 21:
                 score -= 10
+        return score
+    
+    def isBlackjack(self):
+        """Returns true if the hand is blackjack, false otherwise"""
+        return self.scoreHand() == 21 and len(self.hand) == 2
+    
+    def isBust(self):
+        """Returns true if there is no way for the hand to have a score below 21, false otherwise"""
+        score = self.scoreHand()
         return not (score <= 21)
 
+    def __str__(self):
+        return str([str(i) for i in self.hand])
+
+RESHUFFLE_CARD = Card(-1, -1)
 
 ##############################
 # BLACKJACK CLASS DEFINITION #
 ##############################
-class Blackjack:
-    def __init__(self, no_decks):
-        deck = []
-        for _ in no_decks:
-            deck += self.newDeck()
-        self.deck = deck
+MIN_BET = 2
+MAX_BET = 500
 
-    def newDeck(self, shuffle=True):
+class Blackjack:
+    """Blackjack class contains:
+        - deck ([Card]): a deck of n*52 cards
+        - no_decks (int): the number of decks in the shoe
+        - played ([Card]): a list of all cards played during this round (can later be used for counting purposes)
+
+        Player variables:
+        - player_hand (Hand): contains all the cards in the player's hand
+        - player_money (int): the money currently in possesion of the player
+        - player_bet (int): how much the player is betting in a round
+        
+        Dealer variables:
+        - dealer_hand (Hand)
+        - dealer_reshuffle (bool): true if the dealer should reshuffle at the end of the round"""
+    def __init__(self, no_decks=1):
+        # Initialise deck for the game
+        deck = []
+        for _ in range(no_decks):
+            deck += self.newDeck()
+        self.shoe = deck
+        random.shuffle(self.shoe)
+        self.no_decks = no_decks
+
+        # Initialise list of played cards
+        self.played = []
+
+        # Initialise variables for the player
+        self.player_hand = Hand()
+        self.player_money = 200
+        self.player_bet = 0
+
+        # Initialise variables for the dealer
+        self.dealer_hand = Hand()
+        self.dealer_reshuffle = False
+
+
+    def newDeck(self):
+        """Generates a new deck containing all 52 basic cards (so no jokers)"""
         deck = []
         for suit in range(0,4):
             for rank in range(1,14):
                 deck.append(Card(suit, rank))
         return deck
     
+    def drawCard(self):
+        """Takes the last card (the top card) from the deck and returns it.
+           Also adds this card to the played deck."""
+        drawn = self.shoe[-1]
+        self.shoe = self.shoe[:-1]
+        self.played.append(drawn)
+        if drawn == RESHUFFLE_CARD:
+            self.dealer_reshuffle = True
+            drawn = self.drawCard()
+        return drawn
+    
+    def payout(self, verbose=False):
+        player_score = self.player_hand.scoreHand()
+        dealer_score = self.dealer_hand.scoreHand()
+
+        pay = 0
+        if self.player_hand.isBlackjack() and self.dealer_hand.isBlackjack():
+            pay = 0
+            print('Both player and dealer have Blackjack, so it\'s a tie!') if verbose else {}
+        elif self.player_hand.isBlackjack():
+            pay = int(1.5 * self.player_bet)
+            print('Player has Blackjack, so is awarded 1.5 times their bet!') if verbose else {}
+        elif not self.player_hand.isBust() and self.dealer_hand.isBust():
+            pay = self.player_bet
+            print('Dealer has gone bust and player has not, so player wins!') if verbose else {}
+        elif self.player_hand.isBust():
+            pay = -self.player_bet
+            print('Player has gone bust, so the dealer wins!') if verbose else {}
+        elif player_score > dealer_score:
+            pay = self.player_bet
+            print('Player has a higher hand than the dealer, so the player wins!') if verbose else {}
+        elif player_score < dealer_score:
+            pay = -self.player_bet
+            print('Dealer has a higher hand than the player, so the dealer wins!') if verbose else {}
+        elif player_score == dealer_score:
+            pay = 0
+            print('Dealer and player have the same score, so it\'s a tie!') if verbose else {}
+        else:
+            raise Exception('Unknown case hit during payout! Check what happened')
+        
+        self.player_money += pay
+        if verbose and pay > 0:
+            print(f'Player won €{pay}.')
+        elif verbose and pay < 0:
+            print(f'Player lost €{pay}.')
+        return pay     
+    
+    def playGameInteractive(self):
+        rounds = 1
+        while self.player_money > MIN_BET:
+            # Round start
+            print(f'--------------\nROUND {rounds}\n--------------')
+            print(f'Drawing a card: {self.drawCard()}')
+            self.player_hand = Hand([self.drawCard(), self.drawCard()])
+            self.dealer_hand = Hand([self.drawCard(), self.drawCard()])
+            print(f'Dealer\'s up card: {self.dealer_hand.hand[0]}')
+            print(f'Players\'s hand: {str(self.player_hand)}')
+
+            # Bet placement
+            self.player_bet = 0
+            while self.player_bet < MIN_BET or self.player_bet > MAX_BET:
+                self.player_bet = int(input(f'Place your bets! (You have €{self.player_money})\n> '))
+                if self.player_bet < MIN_BET:
+                    print(f'Bet too low! Minimum bet size is {MIN_BET}')
+                elif self.player_bet > MAX_BET:
+                    print(f'Bet too high! Maximum bet size is {MAX_BET}')
+            
+            # Player plays their round 
+            playing = True
+            def player_hits():
+                drawn = self.drawCard()
+                self.player_hand.addCard(drawn)
+                print(f'You drew a {drawn}!')
+                print(f'Player\'s hand: {self.player_hand}')
+                if self.player_hand.isBust():
+                    nonlocal playing 
+                    playing = False
+                    print(f'Your hand is bust!\n')
+
+            def player_stands():
+                nonlocal playing
+                print(f'Player stands on {self.player_hand.scoreHand()}.\n')
+                playing = False
+
+            if self.player_hand.isBlackjack():
+                print(f'You have Blackjack!')
+                playing = False
+
+            while playing:
+                match input(f'Do you want to hit (h) or stand (s)?\n> '):
+                    case 'h'|'H': player_hits()
+                    case 's'|'S': player_stands()
+                    case _:       print('Please input either \'h\' or \'s\'.')
+            
+            # Dealer plays their round
+            playing = True
+            print(f'Dealer\'s hand: {self.dealer_hand}')
+            if self.dealer_hand.isBlackjack():
+                print('Dealer has Blackjack!')
+            while playing:
+                dealer_score = self.dealer_hand.scoreHand()
+                if dealer_score <= 16:
+                    drawn = self.drawCard()
+                    self.dealer_hand.addCard(drawn)
+                    print(f'Dealer drew a {drawn}!\nDealer\'s hand: {self.dealer_hand}')
+                elif dealer_score > 16 and not self.dealer_hand.isBust():
+                    print(f'Dealer stands on {dealer_score}!')
+                    playing = False
+                else:
+                    print(f'Dealer is bust!')
+                    playing = False
+            
+            # Payout
+            self.payout(verbose=True)
+            print('')
+
     def __str__(self):
         string = "Game state:\n" + str([str(card) for card in self.deck])
         return string
 
+state = Blackjack()
+state.playGameInteractive()
